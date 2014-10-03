62b78c033b506e26ead3520c8023f0c8dc4b28e4
thedersen <thedersen@gmail.com>
2014-10-03 11:02:43 +0200

    Added child component

---
+++ app/components/clock.js
@@ -0,0 +1,10 @@
+/** @jsx React.DOM */
+'use strict';
+
+var React = require('react');
+
+module.exports = React.createClass({
+  render: function () {
+    return <div>{this.props.time}</div>;
+  }
+});
--- app/components/main.js
+++ app/components/main.js
@@ -1,10 +1,28 @@
 /** @jsx React.DOM */
 'use strict';
 
-var React = require('react');
+var React = require('react'),
+    clock = require('./clock');
 
 module.exports = React.createClass({
+
+  getInitialState: function () {
+    return {
+      time: Date.now()
+    };
+  },
+
+  handleClick: function () {
+    this.setState({time: Date.now()});
+  },
+
   render: function () {
-    return <h1>Hello Future!</h1>;
+    return (
+      <div>
+        <h1>Hello Future!</h1>
+        <clock time={this.state.time} />
+        <button onClick={this.handleClick}>Click Me</button>
+      </div>
+    );
   }
 });
