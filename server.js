eb8a1915c36aac09dd5302549ed90d30ae2a77b1
thedersen <thedersen@gmail.com>
2015-02-13 13:07:01 +0100

    Replaced ejs with react

--- server.js
+++ server.js
@@ -3,23 +3,31 @@ require('dotenv').load();
 require('node-jsx').install({extension: '.jsx'});
 
 var express = require('express'),
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    pkg = require('./package.json'),
     React = require('react'),
-    mainComponent = React.createFactory(require('./app/components/Main.jsx'));
+    HtmlComponent = React.createFactory(require('./app/HtmlComponent.jsx')),
+    MainComponent = React.createFactory(require('./app/components/Main.jsx'));
 
 var app = express();
-app.set('view engine', 'ejs');
-
-app.use(require('./middleware/locals'));
 
 app.use('/assets', express.static('./public/assets'));
 if(process.env.NODE_ENV !== 'production'){
   app.use('/assets', express.static('./.tmp/public/assets'));
 }
+app.use(cookieParser());
+app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-  res.render('index', {
-    markup: React.renderToString(mainComponent())
-  });
+app.use(function (req, res, next) {
+  var html = React.renderToStaticMarkup(new HtmlComponent({
+    appVersion: pkg.version,
+    baseUrl: process.env.BASE_URL || '',
+    title: 'Isomorphic Web',
+    markup: React.renderToString(new MainComponent())
+  }));
+
+  res.send(html);
 });
 
 var server = app.listen(process.env.PORT || 8000, function() {
