667594a5bef00c445a7c14a0ae930b7e361c521d
John St. Clair <john.stclair@miles.no>
2015-09-04 15:19:39 +0200

    Convert Html

--- app/HtmlComponent.jsx
+++ app/HtmlComponent.jsx
@@ -1,23 +1,20 @@
-'use strict';
-var React = require('react');
+import React from 'react';
 
-var Html = React.createClass({
-  render: function() {
+export default class Html extends React.Component {
+  render() {
     return (
       <html>
         <head>
           <meta charSet="utf-8" />
           <title>{this.props.title}</title>
           <meta name="viewport" content="width=device-width, user-scalable=no" />
-          <link rel="stylesheet" href={this.props.baseUrl + '/assets/main-' + this.props.appVersion + '.min.css'} />
+          <link rel="stylesheet" href={`${this.props.baseUrl}/assets/main-${this.props.appVersion}.min.css`} />
         </head>
         <body>
           <div id="app-container" dangerouslySetInnerHTML={{__html: this.props.markup}}></div>
         </body>
-        <script src={this.props.baseUrl + '/assets/bundle-' + this.props.appVersion + '.min.js'} defer></script>
+        <script src={`${this.props.baseUrl}/assets/bundle-${this.props.appVersion}.min.js`} defer></script>
       </html>
     );
   }
-});
-
-module.exports = Html;
+}
