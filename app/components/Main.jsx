fc5b1b2893fcd6f75e99e8b931bd15ae765b0bd6
John St. Clair <john.stclair@miles.no>
2015-11-28 12:52:56 +0100

    [FIX] fix click bug

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -12,7 +12,7 @@ export default class extends React.Component {
       <div>
         <h1>Hello Future!</h1>
         <Clock time={this.state.time} />
-        <button onClick={() => this.handleClick}>Click Me</button>
+        <button onClick={this.handleClick.bind(this)}>Click Me</button>
       </div>
     );
   }
